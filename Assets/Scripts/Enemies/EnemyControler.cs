e1f2f974e42ff972baef03aa291a7ee37d0b4bad
blizary <rosajunior05@gmail.com>
2020-11-13 17:09:57 +0000

    Enemy spawn and chase player

---
+++ Assets/Scripts/Enemies/EnemyControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");//needs replacement in the future to acomodate 2 players
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChaseTarget();
+    }
+
+    void ChaseTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float respawnTimer;
+    public GameObject enemyPrefab;
+
+    private float irespawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        irespawnTimer = respawnTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawner();
+    }
+
+
+    void Spawner()
+    {
+        if(irespawnTimer<=0)
+        {
+            irespawnTimer = respawnTimer;
+            //spawn mob
+            GameObject newMob = Instantiate(enemyPrefab, this.transform.position, Quaternion.identity, this.transform);
+            Debug.Log("spawning");
+
+        }
+        else
+        {
+            irespawnTimer -= Time.deltaTime;
+        }
+    }
+}
