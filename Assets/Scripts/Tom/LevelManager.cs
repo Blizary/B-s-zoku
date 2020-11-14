bf3de451d7d3ff6404480a8291e1c1d6899aafbd
TomLaptop <Tomcoeckelbergh7@gmail.com>
2020-11-14 20:50:51 +0000

    Added in MainMenu and MainGame scenes, added in MainMenu functionality (Start Game, Quit, Display Information)

---
+++ Assets/Scripts/Tom/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Stuff to hide/show with the information button
+    [SerializeField] private Text _information;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("1_MainGame");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit request initiated - Only works in builds");
+        Application.Quit();
+    }
+
+    public void ShowInformation()
+    {
+        // Show Information Stuff
+        _information.gameObject.SetActive(!_information.gameObject.activeSelf);
+    }
+}
